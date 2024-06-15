f295faa4e6f7f19203e58cda859ab07d1805e450
Erick Lie <ericklie95a@gmail.com>
2024-06-15 14:39:08 +0930

    Resturcture all functions.

--- src/main.py
+++ src/main.py
@@ -3,37 +3,33 @@
 import face_recognition
 import os
 import numpy as np 
+from datetime import datetime
     
 def initialise():
     '''
-    Make directory if not found.
-    
-    Tested on Windows 10.
-    
-    Enhancement:
-    1. Ensure this is also working on Linux.
+    Make directory in current working directory.
     '''
-    currentWorkingDirectory = os.getcwd()
+    # Enhancement: Use of os.path.exists instead of try except.
     
-    knownPicDir = currentWorkingDirectory + "\\known_face"
+    knownPicDir = os.path.join(os.getcwd(), "known_face")
     try:
         os.makedirs(knownPicDir)
     except FileExistsError:
         pass # directory exists.
     
-    unknownPicDir = currentWorkingDirectory + "\\unknown_face"
+    unknownPicDir = os.path.join(os.getcwd(), "unknown_face")
     try:
         os.makedirs(unknownPicDir)
     except FileExistsError:
         pass # directory exists.
     
-    noFaceFoundDir = currentWorkingDirectory + "\\face_not_found"
+    noFaceFoundDir = os.path.join(os.getcwd(), "face_not_found")
     try:
         os.makedirs(noFaceFoundDir)
     except FileExistsError:
         pass # directory exists.
     
-    sortedDir = currentWorkingDirectory + "\\post_run_face_match"
+    sortedDir = os.path.join(os.getcwd(), "post_run_face_match")
     try:
         os.makedirs(sortedDir)
     except FileExistsError:
@@ -45,7 +41,7 @@ def isFaceFound(imageFile):
     '''
     Check the imageFile if face is found by using face_location method.
     
-    Param: imageFile = image file.
+    Param: [Required] imageFile = image file.
     
     Return:
         True = Image found.
@@ -57,79 +53,55 @@ def isFaceFound(imageFile):
         return False
     return True
 
-def removeFaceless(filename, unknownPicDir, noFaceFoundDir):
-    '''
-    Check if face is found on the chosen file. If face is not found, move it to directory called noFaceFoundDir.
-    '''
-    os.rename(unknownPicDir+"\\"+filename, noFaceFoundDir+"\\"+filename)
-    #print("Move the file: "+ filename)
+def moveFile(filename, pathToSourceDir, pathToDestDir):
+    # Move filename from pathToSourceDir to pathToDestDir    
+    os.rename(os.path.join(pathToSourceDir,filename), os.path.join(pathToDestDir,filename))
 
 def findMatchingFace(knownFaceDict, unknownFaceDict, sortedDir, unknownPicDir):
     '''
-    face_recognition.compare_faces(List x, numpy.ndarray y)
-    x is List of numpy.ndarray
-    y is numpy.ndarray
-    
-    To convert list to numpy.ndarray:
-    array1 = np.asarray(list1)
-    
-    Currently, faceFilenameDict keys are tuples.
-    So, change tuples to list.
-    Then convert list to numpy.ndarray.
-    
-    Possible enhancement:
-    1. Recognition of face that is found but not known yet then add it to the known face.
-    2. Skip iterate over file that already loaded from json file.
-    '''
-    for i in knownFaceDict:
-        known_face_name = knownFaceDict[i] # Get file name of known face. This will include extension.
-        known_face_filename = os.path.splitext(known_face_name)[0] # extract just the filename withotu extension.
-        knownList = []
-        knownList.append(np.asarray(i)) # Assume there is only one face.
-        for j in unknownFaceDict:
-            fileMovedFlag = False
-            check_fileName = unknownFaceDict[j]
-            myList = []
-            myList.append(np.asarray(j))
-            for h in myList:
-                result = face_recognition.compare_faces(knownList, h)
-                if True in result:
-                    try:
-                        os.makedirs(sortedDir+"\\"+known_face_filename)
-                    except FileExistsError:
-                        pass # directory exists.
-                    #print('Filename: {0} have the result: {1}.'.format(knownPic_filename, result)) # testing purposes.
-                    os.rename(unknownPicDir+"\\"+check_fileName, sortedDir+"\\"+known_face_filename+"\\"+check_fileName) # Move known face to their directory
-                    print("File moved to {0}".format(sortedDir+"\\"+known_face_filename+"\\"+check_fileName))          
-                    fileMovedFlag = True
-                    break
-                    
-            '''
-            if fileMovedFlag: # With assumption that there are pictures with multiple faces in it.
-                # Delete all dictionary entry that have the value as check_fileName
-                # use of dict comprehension
-                unknownFaceDict = {key:val for key, val in unknownFaceDict.items() if val != check_fileName}
-            '''
+    Description: Iterate through all known faces and compare to each pictures that need to be identified/compared.
 
-def findMatchingFace2(knownFaceDict, unknownFaceDict, sortedDir, unknownPicDir):
+    Param:
+    [Required] knownFaceDict = dictionary of all known faces encoded.
+    [Required] unknownFaceDict = dictionary of all unknown faces encoded.
+    [Required] sortedDir = path to directory where matching faces stored.
+    [Required] unknownPicDir = path to directory where the unknown pictures are located.
+    '''
+    
+    # Check all encodings for known faces.
     for i in knownFaceDict:
-        known_face_name = knownFaceDict[i] # Get file name of known face. This will include extension.
-        known_face_filename = os.path.splitext(known_face_name)[0] # extract just the filename withotu extension.
+        # Get file name of known face. This will include extension.
+        known_face_name = knownFaceDict[i] 
+        
+        # Extract filename without extension.
+        known_face_filename = os.path.splitext(known_face_name)[0]
+        
+        # Put the encoding to a list, with assumption there is only one face.
         knownList = []
-        knownList.append(np.asarray(i)) # Assume there is only one face.
+        knownList.append(np.asarray(i))
+        
+        # Loop through all encoded pictures within dictionary.
         for j in unknownFaceDict:
-            fileMovedFlag = False
+            # Get file name of known face. This will include extension.
             check_fileName = unknownFaceDict[j]
+            
+            # Convert the value back to ns.numpyndarray, required for compare_faces method.
             h = np.asarray(j)
+            
+            # Compare faces.
             result = face_recognition.compare_faces(knownList, h)
+            
+            # When face matches
             if True in result:
+            
+                # Create directory of sortedDir if directory does not exist.
+                if not os.path.exists(os.path.join(sortedDir, known_face_filename)):
+                    os.makedirs(os.path.join(sortedDir, known_face_filename))
+                
+                # Move the face to the specified directory.
                 try:
-                    os.makedirs(sortedDir+"\\"+known_face_filename)
-                except FileExistsError:
-                    pass # directory exists.
+                    #TESTING: the following is where the movement happens.
                     
-                try:
-                    #TESTING, the following is where the movement happens.
                     #os.rename(unknownPicDir+"\\"+check_fileName, sortedDir+"\\"+known_face_filename+"\\"+check_fileName) # Move known face to their directory
                     print("File moved to {0}".format(sortedDir+"\\"+known_face_filename+"\\"+check_fileName))          
                 except:
@@ -142,87 +114,145 @@ def getAllValues(dictionary):
  
 def convertToFile(dictionary, filename):
     import pickle
-    
+        
     fp = open(filename, 'wb')
     pickle.dump(dictionary, fp)
     fp.close()
  
 
-def encodeAllPics(checkDir, noFaceFoundDir, savedDict={}, dbFile):
+def encodeAllPics(pathToSourceDir, pathToDestDir, savedDict={}, dumpDirectory=""):
     '''
     Param:
-    checkDir = Directory that is going to be checked.
-    noFaceFoundDir = Directory where no face found is going to be stored.
-    knownFaceListName = List of filename from saved file.
+    [Required] pathToSourceDir = Path to directory for images needed to be encoded.
+    [Required] pathToDestDir = Directory where no face found is going to be stored.
+    [Optional] savedDict = Saved dictionary of known faces
+    [Optional] dumpDirectory = Path to directory where the encoding will be dumped every 10 pictures.
 
     Return: dictionary
     key: tuple of numpy.ndarray
     value: name of file
     '''
+    
     returnDict = {}
+    # tmpDict meant to only have up 10 entries at all time.
+    tmpDict = {}
     knownFaceListName = []
     counter = 0
+    
+    # If we have a saved dictionary
     if(savedDict):
         returnDict = savedDict
         knownFaceListName = getAllValues(savedDict)
-    for check_fileName in os.listdir(checkDir):
-        if(check_fileName in knownFaceListName):
-            print("Found {0}, skip to the next file".format(check_fileName))
+        
+    for filename in os.listdir(pathToSourceDir):
+        # Skip if filename already listed within savedDict.
+        if(filename in knownFaceListName):
+            print("Found {0}. Skipping..".format(filename))
             continue
-        # print("Current file check: {0} \\ {1}.".format(checkDir, check_fileName))
-        if not isFaceFound(checkDir + "\\" + check_fileName):
-            removeFaceless(check_fileName, checkDir, noFaceFoundDir)
+            
+        
+        # Move image to another directory if face is not found.
+        pathToFile = os.path.join(pathToSourceDir, filename)
+        if not isFaceFound(pathToFile):
+            moveFile(filename, pathToSourceDir, pathToDestDir)
         else:
-            parsed_image = face_recognition.load_image_file(checkDir+"\\"+check_fileName)
+            parsed_image = face_recognition.load_image_file(pathToFile)
             parsed_encoding = face_recognition.face_encodings(parsed_image)    
             for i in parsed_encoding:
-                tuppleArr = tuple(i) # Convert numpy.ndarray to tuple for hash key.
-                returnDict[tuppleArr] = check_fileName
+                tuppleArr = tuple(i) 
+                # Update tmpDictionary.
+                tmpDict[tuppleArr] = filename
             counter += 1
-        if(counter == 10):
-            counter = 0
-            convertToFile(returnDict, dbFile) 
+            
+        # After every 10 pictures & dump the result.
+        if(counter % 10 == 0):
+            # Get the current date and time
+            current_datetime = datetime.now()
+
+            # Format the date and time as a string
+            formatted_datetime = current_datetime.strftime("%Y-%m-%d_%H-%M-%S")
+            
+            # Concatenate the filename
+            pathToDirectory = os.path.join(os.getcwd(), dumpDirectory)
+            newDbFile = os.path.join(pathToDirectory, formatted_datetime)
+
+            # Convert the tmpDictionary to file.
+            convertToFile(tmpDict, newDbFile)
+            
+            # Update the main dictionary with the temporary dictionary.
+            returnDict.update(tmpDict)
+            
+            # Reset tmpDictionary
+            tmpDict = {}
+            
     return returnDict
 
 
-def loadFromFile(filename):
+def loadFromFile(pathToFile):
     import pickle
     
     try:
-        dbfile = open(filename, 'rb')
+        dbfile = open(pathToFile, 'rb')
         returnDict = pickle.load(dbfile)
         dbfile.close()
         return returnDict
     except:
-        return "File:{0} not found! Exitting...".format(filename)
-        raise SystemExit
- 
+        print("{0} not found!".format(pathToFile))
+        return {}
+        
+def loadFromDir(directoryName):
+    '''
+    param: [Required] directoryName : String.
+    '''
+    returnDict = {}
+    
+    # Join the current directory path with the new directory name
+    pathToDir = os.path.join(os.getcwd(), directoryName)
+    
+    # Check if the directory already exists
+    if not os.path.exists(pathToDir):
+        os.makedirs(pathToDir)
+        print("{0} not found! Creating it..".format(pathToDir))
 
+    # Read all files and combined dictionaries
+    for i in os.listdir(pathToDir):
+        tmpDict = loadFromFile(os.path.join(pathToDir,i))
+        returnDict.update(tmpDict)
+        
+    return returnDict
+    
 def main():
     print("Running Face Recognition")
     knownPicDir, unknownPicDir, noFaceFoundDir, sortedDir = initialise()
 
-    filenameForKnownFace = os.getcwd() + "\\" + "knownFaceDict.db"
-    filenameForUnknownFace = os.getcwd() + "\\" + "unknownFaceDict.db"
+    # Current hard-coded values for creating files and directory.
+    filenameForKnownFace = "knownFaceDict.db"
+    filenameForUnknownFace = "unknownFaceDict.db"
+    unknownFaceDbDir = "unknownfacedb"
     
-    # Assumption: If filename is the same, we assume the encoding has already been completed.
-    savedKnownFaceDict = loadFromFile(filenameForKnownFace)
+    pathToKnownDbFile = os.path.join(os.getcwd(), filenameForKnownFace)
+    savedKnownFaceDict = loadFromFile(pathToKnownDbFile)
     if(not savedKnownFaceDict):
         savedKnownFaceDict = {}
-    savedUnknownFaceDict = loadFromFile(filenameForUnknownFace)
+        
+    pathToKnownDbFile = os.path.join(os.getcwd(), filenameForUnknownFace)
+    savedUnknownFaceDict = loadFromFile(pathToKnownDbFile)
+    pathToKnownDbDir = os.path.join(os.getcwd(), unknownFaceDbDir)
+    savedUnknownFaceDict = loadFromDir(pathToKnownDbDir)
     if(not savedUnknownFaceDict):
         savedUnknownFaceDict = {}
     
-    # Encode all known pictures and store it to a file.
-    knownFaceDict = encodeAllPics(knownPicDir, noFaceFoundDir, savedKnownFaceDict, filenameForKnownFace)
+    # Encode all known pictures and store it to a file when finished.
+    knownFaceDict = encodeAllPics(knownPicDir, noFaceFoundDir, savedKnownFaceDict)
     convertToFile(knownFaceDict, filenameForKnownFace) 
 
-    # Encode all unknown pictures and store it to a file.
-    unknownFaceDict = encodeAllPics(unknownPicDir, noFaceFoundDir, savedUnknownFaceDict, filenameForUnknownFace)
-    convertToFile(unknownFaceDict, filenameForUnknownFace)
+    # Encode all unknown pictures and store it to a file when finished.
+    unknownFaceDict = encodeAllPics(unknownPicDir, noFaceFoundDir, savedUnknownFaceDict, unknownFaceDbDir)
+    pathToFile = os.path.join(pathToKnownDbDir, filenameForUnknownFace)
+    convertToFile(unknownFaceDict, pathToFile)
     
     # Find matching face based on the 2 encodings that we have done.
-    findMatchingFace2(knownFaceDict, unknownFaceDict, sortedDir, unknownPicDir)    
+    findMatchingFace(knownFaceDict, unknownFaceDict, sortedDir, unknownPicDir)    
         
     print("Finish running.")
 
@@ -232,6 +262,3 @@ if __name__ == "__main__":
 def getKeyFromVal(dictionary, value):
     key = list(dictionary.keys())[list(dictionary.values()).index(value)]
     return key
-    
-
-    
